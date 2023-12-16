414dfa009be186d757ceccac647627a03e8ce35a
kedaya-blue <52734343+kedaya-blue@users.noreply.github.com>
2023-12-16 10:31:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "iostream"
+using namespace std;
+
+double Factorial(int n) {
+	double f=1;
+	for (int i = 1; i < n + 1; i++) {
+		f *= i;
+	}
+	return f;
+}
+static double Fomula(int n,int x) {
+	double m = 1;
+	double ans;
+	while (n > 0&&pow(x,n)/double(Factorial(n))>0.000001) {
+		ans = pow(x, n) / double(Factorial(n));
+		m = m + ans;
+		n--;
+	}
+	return m;
+}
+int main()
+{
+	int n;
+	int x=0;
+	cin >> n>>x;
+	cout<<Fomula(n,x);
+}
